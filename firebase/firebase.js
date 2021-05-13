1e7037d53867b7a8fd18cae79ce19a6804618554
s5227170 <56725628+s5227170@users.noreply.github.com>
2021-05-13 19:19:18 +0100

    Security update
    Removed API keys for security reasons

--- firebase/firebase.js
+++ firebase/firebase.js
@@ -1,13 +1,14 @@
 import * as firebase from 'firebase'
 
+//Include firebase project details
 firebase.initializeApp({
-    apiKey: "AIzaSyDzv1S7W-tsIQGeFPwF7LJcYlZHG0-PSbc",
-    authDomain: "pmt-proj.firebaseapp.com",
-    databaseURL: "https://pmt-proj-default-rtdb.europe-west1.firebasedatabase.app",
-    projectId: "pmt-proj",
-    storageBucket: "pmt-proj.appspot.com",
-    messagingSenderId: "659020703515",
-    appId: "1:659020703515:web:07c1e1b088198a395d55be"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
 }) 
 
 export default firebase;
